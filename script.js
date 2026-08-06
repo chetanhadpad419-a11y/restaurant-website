9548129460b2d120628b26f69e44dff8e3aef09e
Chetan <chetanhadpad419@gmail.com>
2026-08-06 11:39:28 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Welcome message
+console.log("Welcome to Foodie Restaurant!");
+
+// Order button
+const orderButton = document.querySelector("button");
+
+orderButton.addEventListener("click", () => {
+  alert("🍽️ Thank you for choosing Foodie Restaurant!\nYour order request has been received.");
+});
